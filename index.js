85cee05304661a01f9c6c82a15c9266a5f6b148f
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-07-21 22:21:55 +0600

    List Item API Created

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-// middleware
-app.use(cors());
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+}
+
+
+// middleWare
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cu5hws3.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const allProductsCollection = client.db("ElectraStore").collection("Products");
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get("/all-products", async (req, res)=>{
+      const result = await allProductsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/products', async (req, res) => {
+      const email = req.params.email;
+      const product = req.body;
+      const result = await allProductsCollection.insertOne(product);
+      res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 
